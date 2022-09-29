2f2390e66014bb0d0557f726566dc0315aa900dc
Thomas Lau <thomas.lau@gfo-x.com>
2022-09-29 14:54:01 +0800

    found out some dependency package

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ from setuptools import setup, find_packages
 
 __author__ = 'David Barroso <dbarrosop@dravetech.com>'
 
+with open("requirements.txt", "r") as fs:
+    reqs = [r for r in fs.read().splitlines() if (len(r) > 0 and not r.startswith("#"))]
 
 setup(
     name="napalm-fsos",
@@ -21,5 +23,6 @@ setup(
         'Operating System :: POSIX :: Linux',
     ],
     url="https://github.com/napalm-automation-community/napalm-fsos",
-    include_package_data=True
+    include_package_data=True,
+    install_requires=reqs,
 )
